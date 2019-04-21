a0a7ccaccc5adbbbb4dcd20e2a6a59af9a09c595
Mario-Bx <>
2019-04-20 22:51:46 -0500

    Potenciacion de perfil Administrador

---
+++ src/main/webapp/js/FgTarjetaEstu.js
@@ -0,0 +1,256 @@
+/* 
+ * Funcion que pinta las tarjetas de los estudaintes
+ */
+
+var api = "http://localhost:8090/indexRest/rest/";
+
+function listaTarjetaEst() {
+    $.ajax({
+        url: api + "EstudiantesSv",
+        type: "Get",
+        success: function (data) {
+            console.log("Servicio Listar EstudiantesSv");
+            console.log(data);
+            
+            if (data !== undefined) {
+                PintarTarjetaEstu(data);
+            }else{
+                console.log("No Has Creado Convocatorias" );
+            }
+
+        },
+        error: function () {
+            alert('Error function listaEstudiLista');
+            console.log("Error Servicio Listar EstudiantesSv");
+        }
+    });
+
+}
+
+function PintarTarjetaEstu(listasDatos) {
+    console.log("Pintar la Lista de EstudiantesSv");
+    $("#TarjetaEst").empty();
+
+    if (listasDatos instanceof  Array !== false) {
+        listasDatos.EstudiantesDTO.forEach(element => {
+            $("#TarjetaEst").append(
+                    '<hr>'+
+                    '<div class="blog-card">'+
+                        '<div class="meta">'+
+                            '<div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"></div>'+
+                                '<ul class="details w3-medium w3-center">'+
+                                    '<li class="fa fa-graduation-cap w3-margin-top"><a href="#">'+element.Escuela+'</a></li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-calendar w3-margin-top">'+element.FechaNacimiento+'</li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-comments w3-margin-top">'+element.Correo+'</li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-phone w3-margin-top">'+
+                                        '<ul>'+
+                                            '<li><a href="#">'+element.Telefono+' - </a></li>'+
+                                            '<li><a href="#">'+element.Celular+'</a></li>'+
+                                        '</ul>'+
+                                    '</li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-drivers-license-o w3-margin-top">'+
+                                        '<ul>'+
+                                            '<li>'+element.TipoDocumento+': </li>'+
+                                            '<li>'+element.Documento+'</li>'+
+                                        '</ul>'+
+                                    '</li>'+
+                                '</ul>'+
+                        '</div>'+
+                            '<div class="description">'+
+                                '<h1>'+element.Nombre+' '+element.Apellidos+'</h1>'+
+                                '<h2>'+element.Carreara+' ('+element.Semestre+')</h2>'+
+                                '<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>'+
+                                '<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>'+
+                                '<p class="read-more">'+
+                                    '<a href="#">Mas Detalles</a>'+
+                                '</p>'+
+                            '</div>'+
+                    '</div>'
+                    );
+        });
+    }
+    else{
+        $("#TarjetaEst").append(
+                    '<hr>'+
+                    '<div class="blog-card">'+
+                        '<div class="meta">'+
+                            '<div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"></div>'+
+                                '<ul class="details w3-medium w3-center">'+
+                                    '<li class="fa fa-graduation-cap w3-margin-top"><a href="#">'+listasDatos.Escuela+'</a></li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-calendar w3-margin-top">'+listasDatos.FechaNacimiento+'</li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-comments w3-margin-top">'+listasDatos.Correo+'</li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-phone w3-margin-top">'+
+                                        '<ul>'+
+                                            '<li><a href="#">'+listasDatos.Telefono+' - </a></li>'+
+                                            '<li><a href="#">'+listasDatos.Celular+'</a></li>'+
+                                        '</ul>'+
+                                    '</li>'+
+                                    '<br>'+
+                                    '<li class="fa fa-drivers-license-o w3-margin-top">'+
+                                        '<ul>'+
+                                            '<li>'+listasDatos.TipoDocumento+': </li>'+
+                                            '<li>'+listasDatos.Documento+'</li>'+
+                                        '</ul>'+
+                                    '</li>'+
+                                '</ul>'+
+                        '</div>'+
+                            '<div class="description">'+
+                                '<h1>'+listasDatos.Nombre+' '+listasDatos.Apellidos+'</h1>'+
+                                '<h2>'+listasDatos.Carreara+' ('+listasDatos.Semestre+')</h2>'+
+                                '<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>'+
+                                '<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>'+
+                                '<p class="read-more">'+
+                                    '<a href="#">Mas Detalles</a>'+
+                                '</p>'+
+                            '</div>'+
+                    '</div>'
+                    );
+    }
+}
+
+
+function FormEstudiente() {
+    $.ajax({
+        url: api + "AdministradorSv/1",
+        type: "Get",
+        success: function () {
+
+            console.log("*****************");
+            console.log("Funcion Pintar Formulario EstudienteSv");
+            console.log("*****************");
+
+            var m = "'id02'";
+            var b = "'none'";
+
+            $("#id02").empty();
+                $("#id02").append(
+                        '<div class="w3-modal-content w3-card-4 w3-animate-zoom">'+
+                                '<header class="w3-container w3-blue">'+
+                                        '<span onclick="document.getElementById('+m+').style.display='+b+'" class="w3-button w3-blue w3-xlarge w3-display-topright">&times;</span>'+
+                                        '<h2>Editar </h2>'+
+                                '</header>'+
+
+                                '<div class="w3-container">'+
+                                '<br>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Nombre Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control " id="Nombre" placeholder="Ingrese Nombre">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Apellidos Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Apellidos" placeholder="Ingrese Apellidos">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Correo Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Correo" placeholder="Ingrese Correo">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Clave Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Clave" placeholder="Ingrese Clave">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">TipoDocumento Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="TipoDocumento" placeholder="Ingrese TipoDocumento">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Documento Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Documento" placeholder="Ingrese Documento">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">FechaNacimiento Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="FechaNacimiento" placeholder="Ingrese FechaNacimiento">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Celular Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Celular" placeholder="Ingrese Celular">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Telefono Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Telefono" placeholder="Ingrese Telefono">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Escuela Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Escuela" placeholder="Ingrese Escuela">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Carreara Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Carreara" placeholder="Ingrese Carreara">'+
+                                        '</div>'+
+                                        '<div class="form-group">'+
+                                                '<label for="usr">Semestre Estudiantes :</label>'+
+                                                '<input type="text"'+
+                                                 'class="form-control" id="Semestre" placeholder="Ingrese Semestre">'+
+                                        '</div>'+
+                                '</div>'+
+                                
+                                ' <div  class="w3-container w3-center">'+
+                                    '<button class="w3-btn w3-ripple w3-round-xlarge w3-hover-red w3-purple" onclick="crearEstudiantes(), listaTarjetaEst()">Crear</button>'+
+                                '</div>'+
+                                '<br>'+
+                        '<div class="w3-container w3-light-grey w3-padding">'+
+                                '<button class="w3-button w3-right w3-white w3-border" onclick="document.getElementById('+m+').style.display='+b+'">Close</button>'+
+                        '</div>'+
+                '</div>'
+                );
+            
+        },
+        error: function () {
+            alert('Errror El formulario actualizarEstudiante');
+        }
+    });
+}
+
+function crearEstudiantes() {
+
+    console.log("Servicio Crear EstudiantesSv");
+    var Estudiantes = '{' +
+            '"Nombre": "' + $("#Nombre").val() + '"' + ', ' +
+            '"Apellidos": "' + $("#Apellidos").val() + '"' + ', ' +
+            '"Correo": "' + $("#Correo").val() + '"' + ', ' +
+            '"Clave": "' + $("#Clave").val() + '"' + ', ' +
+            '"TipoDocumento": "' + $("#TipoDocumento").val() + '"' + ', ' +
+            '"Documento": "' + $("#Documento").val() + '"' + ', ' +
+            '"FechaNacimiento": "' + $("#FechaNacimiento").val() + '"' + ', ' +
+            '"Celular": "' + $("#Celular").val() + '"' + ', ' +
+            '"Telefono": "' + $("#Telefono").val() + '"' + ', ' +
+            '"Escuela": "' + $("#Escuela").val() + '"' + ', ' +
+            '"Carreara": "' + $("#Carreara").val() + '"' + ', ' +
+            '"Semestre": "' + $("#Semestre").val() + '"' + ', ' +
+            '"Estado": "' + 1 + '"'
+
+            + '}';
+
+    console.log(Estudiantes);
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: api + "EstudiantesSv",
+        data: Estudiantes,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+        },
+        error: function () {
+            alert('Error En el Servicio Crear EstudiantesSv');
+        }
+    });
+}
